3b41a4d36c1dcc95b83c44423635e348878ca9e8
laszlo.bodnar <laszlo.bodnar@instructure.com>
2021-03-25 12:55:34 +0100

    Added metal powder bottles and related assets

---
+++ src/main/java/com/csonyi/cosmerecraft/common/item/MetalVial.java
@@ -0,0 +1,41 @@
+package com.csonyi.cosmerecraft.common.item;
+
+import com.csonyi.cosmerecraft.common.capabilities.allomancy.InvestedMetal;
+import com.csonyi.cosmerecraft.common.capabilities.allomancy.CapabilityAllomancy;
+import com.csonyi.cosmerecraft.common.capabilities.allomancy.IAllomancy;
+import com.csonyi.cosmerecraft.setup.ModSetup;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Food;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.PotionItem;
+import net.minecraft.world.World;
+
+public class MetalVial extends PotionItem {
+  private final InvestedMetal metal;
+  public MetalVial(InvestedMetal metal) {
+    super(new Item.Properties()
+            .group(ModSetup.COSMERECRAFT_GROUP)
+            .maxStackSize(1)
+            .food(new Food.Builder()
+                    .saturation(1)
+                    .setAlwaysEdible()
+                    .fastToEat()
+                    .build()
+            )
+    );
+    this.metal = metal;
+  }
+
+  @Override
+  public ItemStack onItemUseFinish(ItemStack stack, World worldIn, LivingEntity entityLiving) {
+    if(!(entityLiving instanceof PlayerEntity)) return super.onItemUseFinish(stack, worldIn, entityLiving);
+    PlayerEntity player = (PlayerEntity) entityLiving;
+    IAllomancy allomancy = CapabilityAllomancy.getAllomancy(player);
+
+    allomancy.changeReserve(metal, 250F);
+
+    return super.onItemUseFinish(stack, worldIn, entityLiving);
+  }
+}
